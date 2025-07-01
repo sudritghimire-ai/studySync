a608880b27cc68380972419618f5805bdfac02f3
sudritghimire-ai <sudritghimire@gmail.com>
2025-07-01 16:10:23 +0545

    Update axios.js

--- client/src/lib/axios.js
+++ client/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
-
 export const axiosInstance = axios.create({
-	baseURL: BASE_URL,
-	withCredentials: true,
+  baseURL: "https://studysync-mmo6.onrender.com/api",
+  withCredentials: true,
 });
