69d3e3ccdcc384836044b3ebf8ade4b934489c54
sudritghimire-ai <sudritghimire@gmail.com>
2025-07-02 13:44:51 +0545

    Update useSidebarStore.js

--- client/src/store/useSidebarStore.js
+++ client/src/store/useSidebarStore.js
@@ -3,8 +3,11 @@ import { create } from "zustand";
 
 export const useSidebarStore = create((set) => ({
   isDrawerOpen: false,
+  shouldOpenOnHome: false,          // << add this
   toggleDrawer: () =>
     set((state) => ({ isDrawerOpen: !state.isDrawerOpen })),
   openDrawer: () => set({ isDrawerOpen: true }),
   closeDrawer: () => set({ isDrawerOpen: false }),
+  setShouldOpenOnHome: (value) =>   // << add this
+    set({ shouldOpenOnHome: value }),
 }));
